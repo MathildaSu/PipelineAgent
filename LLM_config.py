ee4361dc8105ec35fd0de23045528799e406c98e
MathildaSu <mathildagtst@gmail.com>
2025-02-19 15:42:55 +0000

    added llm config info

--- LLM_config.py
+++ LLM_config.py
@@ -2,12 +2,25 @@ import os
 
 from autogen import config_list_from_json
 
-llm_config = config_list = config_list_from_json(
-    "OAI_CONFIG_LIST",
-    filter_dict={
-        "model": ["gpt4", "gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-v0314"],
+
+config_list = [
+    {
+        "model": "deepseek-r1:7b",
+        "base_url": "http://localhost:11434/v1",
+        "api_key": "ollama",
     },
-)
+    # {
+    #     "model": "claude",
+    #     "base_url": "http://localhost:11434/v1",
+    #     "api_key": "ollama",
+    # },
+#     {
+#     "model": "qwen2.5-coder:7b",
+#     "base_url": "http://localhost:11434/v1",
+#     "api_key": "ollama",
+#   }
+]
+
 
 llm_config = {
     "timeout": 600,
@@ -15,3 +28,14 @@ llm_config = {
     "config_list": config_list,
     "temperature": 0.02,
 }
+
+llm_CDA_config = {
+    "timeout": 600,
+    "cache_seed": 42,
+    "config_list": config_list,
+    "temperature": 0.02,
+    # "max_tokens": 80,  # Force brief responses
+    # "stop": ["\n\n"],  # Prevent verbose output
+
+}
+
